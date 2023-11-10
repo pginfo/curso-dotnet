d48bdd7402fbdbbc4fdf099eccd390bc6fc7eb72
Genildo Martins <genildovsm@gmail.com>
2023-11-10 18:24:46 -0300

    Iniciando a implementação de testes unitários

--- DevFreela/DevFreela.Core/Entities/Project.cs
+++ DevFreela/DevFreela.Core/Entities/Project.cs
@@ -23,17 +23,28 @@ namespace DevFreela.Core.Entities
         }
 
         public string Title { get; private set; }
+
         public string Description { get; private set; }
+
         public int IdClient { get; private set; }
+
         public User Client { get; private set; }
+
         public int IdFreelancer { get; private set; }
+
         public User Freelancer { get; private set; }
+
         [Column(TypeName = "decimal(18,2)")]
         public decimal TotalCost { get; private set; }
+
         public DateTime CreatedAt { get; private set; }
+
         public DateTime? StartedAt { get; private set; }
+
         public DateTime? FinishedAt { get; private set; }
+
         public ProjectStatusEnum Status { get; private set; }
+
         public List<ProjectComment> Comments { get; private set; }
 
         public void Cancel()
---
+++ DevFreela/DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public void ThreeProjectsExists_Executed_ReturnThreeProjectViewModels()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
