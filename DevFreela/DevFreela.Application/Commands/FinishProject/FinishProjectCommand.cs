e90541e6c603e80830acd00b6a7f021e8e1e7890
Genildo Martins <genildovsm@gmail.com>
2023-11-13 17:02:00 -0300

    Iniciando implementação de microserviços

--- DevFreela/DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
+++ DevFreela/DevFreela.Application/Commands/FinishProject/FinishProjectCommand.cs
@@ -2,14 +2,26 @@
 
 namespace DevFreela.Application.Commands.FinishProject
 {
-    public class FinishProjectCommand : IRequest<Unit>
+    public class FinishProjectCommand : IRequest<bool>
     {
 
-        public FinishProjectCommand(int id)
+        public FinishProjectCommand(int id, string creditCardNumber, string cvv, string expiresAt, string fullName)
         {
             Id = id;
+            CreditCardNumber = creditCardNumber;
+            Cvv = cvv;
+            ExpiresAt = expiresAt;
+            FullName = fullName;
         }
 
         public int Id { get; private set; }
+
+        public string CreditCardNumber { get; private set; }
+
+        public string Cvv { get; private set; }
+
+        public string ExpiresAt { get; private set; }
+
+        public string FullName { get; private set; }
     }
 }
